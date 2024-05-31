d27d27d3e06c9d8b19b48a7fb012e5553cdedd39
hamza7681 <hamza.siddique@desolint.com>
2024-05-31 15:33:28 +0500

    MISC-47 Fix: Driver filter error issue on LicenseType fix

--- services/usersServices.js
+++ services/usersServices.js
@@ -293,28 +293,37 @@ module.exports = class UsersServices {
       role: roles.driver.value,
     };
 
+    const andConditions = [];
+
     if (title) {
-      query.$or = [
-        {firstName: {$regex: title, $options: 'i'}},
-        {lastName: {$regex: title, $options: 'i'}},
-      ];
+      andConditions.push({
+        $or: [
+          {firstName: {$regex: title, $options: 'i'}},
+          {lastName: {$regex: title, $options: 'i'}},
+        ],
+      });
+    }
+
+    if (licenseTypes) {
+      andConditions.push({
+        $or: [
+          {'stateLicenses.stateLicenseType': licenseTypes},
+          {'federalLicenses.federalLicenseType': licenseTypes},
+        ],
+      });
+    }
+
+    if (andConditions.length > 0) {
+      query.$and = andConditions;
     }
 
     if (location) {
       query.city = {$regex: location, $options: 'i'};
     }
 
-    if (licenseTypes) {
-      query.$or = [
-        {'stateLicenses.stateLicenseType': licenseTypes},
-        {'federalLicenses.federalLicenseType': licenseTypes},
-      ];
-    }
-
     if (equipment.length > 0) {
       query.handleEquipment = {$in: equipment};
     }
-
     try {
       const totalCount = await UsersModel.find(query).countDocuments();
       const skip = (page - 1) * limit;
