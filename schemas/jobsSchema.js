d2d7ddca279ba59ed1996bf81e47168733397d5a
hamza7681 <hamza.siddique@desolint.com>
2024-06-20 11:29:18 +0500

    MISC-63 Refactor: Job Validation updated

--- schemas/jobsSchema.js
+++ schemas/jobsSchema.js
@@ -27,12 +27,8 @@ const commonFields = {
         .min(1, 'Please select at least 1 equipment type'),
     otherwise: () => Yup.array(),
   }),
-  stateLicenseTypes: Yup.array()
-    .of(Yup.string().oneOf(stateLicenseTypes))
-    .min(1, 'Please select at least 1 state license type'),
-  federalLicenseTypes: Yup.array()
-    .of(Yup.string().oneOf(federalLicenseTypes))
-    .min(1, 'Please select at least 1 state license type'),
+  stateLicenseTypes: Yup.array(),
+  federalLicenseTypes: Yup.array(),
   city: Yup.string().required('City is required'),
   postalCode: Yup.string().required('Postal code is required'),
   description: Yup.string().required('Description is required'),
