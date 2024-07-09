d7ffa0b52200299128fe4b83d5643d0b4f7f318f
hamza7681 <hamza.siddique@desolint.com>
2024-07-09 18:00:38 +0500

    MISC-52 Feat: Structure of factories, v1 and routes created for offers

--- apiVersions/v1.js
+++ apiVersions/v1.js
@@ -5,6 +5,7 @@ const {
   privateRoute,
   jobsRoute,
   othersRoute,
+  offersRoute,
 } = require(`../routes`);
 
 const apiPrefix = '/api/v1/';
@@ -12,6 +13,7 @@ const prepareV1Routes = (app) => {
   app.use(`${apiPrefix}users`, usersRoute); // This route is public mostly and whenever it's not, it's protected by the authMiddleware inside the specific route
   app.use(`${apiPrefix}jobs`, jobsRoute);
   app.use(`${apiPrefix}others`, othersRoute);
+  app.use(`${apiPrefix}offers`, offersRoute);
   app.use(`${apiPrefix}public`, publicRoute);
   app.use(`${apiPrefix}private`, authMiddleware, privateRoute);
 };
