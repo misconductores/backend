8a6eedd9468d2de26baf3cc9d6f7c48695f7a662
Amjad992 <Amjad@desolint.com>
2024-07-11 03:34:18 +0500

    chores: modify sender details

--- values/contants/email.js
+++ values/contants/email.js
@@ -1,5 +1,5 @@
-module.exports.defaultEmailAddress = 'noreply@desolint.com';
-module.exports.defaultEmailName = 'Desol Int.';
+module.exports.defaultEmailAddress = 'no-reply@misconductores.com';
+module.exports.defaultEmailName = 'Misconductores';
 
 module.exports.sendGridResetLoginTemplateId =
   'd-5492a12466a24a44b1d5e3fe3918b857';
