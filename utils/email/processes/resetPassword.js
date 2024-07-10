4b4aa9b04f1080afcd1c76592f0189b62059b5d5
Amjad992 <Amjad@desolint.com>
2024-07-11 03:31:28 +0500

    Chores: modify send grid values to correct ones

--- utils/email/processes/resetPassword.js
+++ utils/email/processes/resetPassword.js
@@ -3,13 +3,8 @@ const {
   defaultEmailName,
   defaultEmailAddress,
   sendGridResetLoginTemplateId,
-  sendGridResetDummyId,
 } = require('../../../values/contants/email');
 const {usersConstants} = require('../../../constants');
-const {
-  SENDGRID_EMAIL,
-  SENDGRID_EMAIL_NAME,
-} = require('../../../constants/usersConstants');
 
 module.exports = async ({user, resetUrl}) => {
   const {email, firstName, lastName, companyName, role} = user;
@@ -22,10 +17,10 @@ module.exports = async ({user, resetUrl}) => {
   }
 
   const to = {email, name};
-  const from = {email: SENDGRID_EMAIL, name: SENDGRID_EMAIL_NAME};
+  const from = {email: defaultEmailAddress, name: defaultEmailName};
 
   // this dummy id will replaced with original one later
-  const templateId = sendGridResetDummyId;
+  const templateId = sendGridResetLoginTemplateId;
   const dynamic_template_data = {name: name, resetUrl};
 
   await sendEmail({to, from, templateId, dynamic_template_data});
--- values/contants/email.js
+++ values/contants/email.js
@@ -2,10 +2,7 @@ module.exports.defaultEmailAddress = 'noreply@desolint.com';
 module.exports.defaultEmailName = 'Desol Int.';
 
 module.exports.sendGridResetLoginTemplateId =
-  'd-5b909e46f6534b92abd6bd2d21397c22';
+  'd-5492a12466a24a44b1d5e3fe3918b857';
 
 module.exports.sendGridVerifyUserTemplateId =
-  'd-a703468d158844edw3b01e8e8a5b6c8a';
-
-module.exports.sendGridDummyTemplateId = 'd-f0dd856f3bda44468299e3f3ae38228c';
-module.exports.sendGridResetDummyId = 'd-5492a12466a24a44b1d5e3fe3918b857';
+  'd-f0dd856f3bda44468299e3f3ae38228c';
