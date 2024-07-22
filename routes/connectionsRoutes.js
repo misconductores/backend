d77a419740c4d3e947857ce76a25236bd996f84e
hamza7681 <hamza.siddique@desolint.com>
2024-07-22 17:50:10 +0500

    MISC-67 Feat: Validations added for data of driver disconnect with company

--- routes/connectionsRoutes.js
+++ routes/connectionsRoutes.js
@@ -5,7 +5,9 @@ const {
   roleValidatorMiddleware,
   findConnectionMiddleware,
   isReviewExistMiddleware,
+  validatorMiddleware,
 } = require('../middleware');
+const {reviewsSchema} = require('../schemas');
 const {catchAsync} = require('../utils');
 
 const router = require('express').Router();
@@ -14,6 +16,7 @@ router.post(
   '/driver-disconnect',
   authMiddleware,
   roleValidatorMiddleware({allowedRoles: [roles.driver.value]}),
+  validatorMiddleware(reviewsSchema.validateDriverDisconnectReq),
   isReviewExistMiddleware,
   findConnectionMiddleware,
   catchAsync(ConnectionsController.disconnectionByDriver)
--- schemas/index.js
+++ schemas/index.js
@@ -2,10 +2,12 @@ const usersSchema = require(`./usersSchema`);
 const jobsSchema = require('./jobsSchema');
 const othersSchema = require('./othersSchema');
 const offersSchema = require('./offersSchema');
+const reviewsSchema = require('./reviewsSchema');
 
 module.exports = {
   usersSchema,
   jobsSchema,
   othersSchema,
   offersSchema,
+  reviewsSchema,
 };
