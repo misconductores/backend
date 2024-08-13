ef892746aad458d5139ee45580eb3ad90f2ce74f
hamza7681 <hamza.siddique@desolint.com>
2024-08-13 20:45:39 +0500

    MISC-85 Refactor: 7 days restriction removed

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const {
 } = require('./middleware');
 const {corsOrigin} = require('./utils');
 const {documentDeleteCronJob} = require('./utils/cron-jobs/documents');
-const {expirePendingOffers} = require('./utils/cron-jobs/offers');
 const {
   expirePendingDocsAccessRequests,
 } = require('./utils/cron-jobs/docAccessRequests');
@@ -60,7 +59,6 @@ app.use(finalResponseMiddleware);
 
 documentDeleteCronJob.start();
 
-expirePendingOffers();
 expirePendingDocsAccessRequests();
 
 if (config.get('env') !== config.get('envVariables.test')) {
--- utils/cron-jobs/docAccessRequests.js
+++ utils/cron-jobs/docAccessRequests.js
@@ -1,7 +1,7 @@
 const {TIMEZONES, statusTypes} = require('../../constants/usersConstants');
 const {DocumentAccessModel} = require('../../models');
 const {CronJob} = require('cron');
-const {getDate7DaysAgo, getCurrentDate} = require('../DateCalculations');
+const {getCurrentDate} = require('../DateCalculations');
 
 exports.expirePendingDocsAccessRequests = async () => {
   const schedule = '0 0 1 * * *'; // at 01:00:00 every day
@@ -11,15 +11,6 @@ exports.expirePendingDocsAccessRequests = async () => {
         {
           status: statusTypes.accepted.value,
           endDate: {$lt: getCurrentDate()},
-        },
-        {
-          $set: {status: statusTypes.expired.value},
-        }
-      );
-      await DocumentAccessModel.updateMany(
-        {
-          status: statusTypes.pending.value,
-          createdAt: {$lt: getDate7DaysAgo()},
         },
         {
           $set: {status: statusTypes.expired.value},
