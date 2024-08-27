9f5450231f249861661d3b38bd95499dbc66e6e8
hamza7681 <hamza.siddique@desolint.com>
2024-08-27 20:06:05 +0500

    MISC-110 Refactor: update driver status moved to controller

--- services/reviewsServices.js
+++ services/reviewsServices.js
@@ -76,27 +76,7 @@ module.exports = class ReviewsServices {
         model: ReviewsModel,
         data: {status: status},
       });
-      if (review.type === reviewTypes.driver_review.value) {
-        const connection = await ConnectionsModel.findOne({
-          _id: review.connectionId,
-        }).populate({path: 'driverId', select: 'driverStatus'});
 
-        if (
-          connection.driverId.driverStatus !==
-            driverStatuses.underInspection.value &&
-          connection.driverId.driverStatus !== driverStatuses.connected.value
-        ) {
-          await GeneralServices.update({
-            id: review.driverId,
-            data: {
-              driverStatus: connection.companyReviewId
-                ? driverStatuses.available.value
-                : driverStatuses.availableSoon.value,
-            },
-            model: UsersModel,
-          });
-        }
-      }
       return {success: true};
     } catch (error) {
       return {success: false, error};
