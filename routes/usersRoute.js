7faeb257b394b63be4688b3245a5befd2fe78f20
Rao Imtinan <2019cs32@stu.mnsuet.edu.pk>
2024-11-06 12:26:21 +0500

    refactor:update role base success configration

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -29,7 +29,9 @@ router.get(
 router.get(
   '/drivers',
   authMiddleware,
-  roleValidatorMiddleware({allowedRoles: [roles.admin.value]}),
+  roleValidatorMiddleware({
+    allowedRoles: [roles.admin.value, roles.company.value],
+  }),
   validatorMiddleware(othersSchema.validatePaginationParams, QUERY_PROPERTY),
   catchAsync(UsersController.getDriversList)
 );
